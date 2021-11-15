f6fd6641d39fb9019c18f8700ec6c8fe4c72d2a2
KhelemeliaOleksii <89631480+KhelemeliaOleksii@users.noreply.github.com>
2021-11-15 09:59:19 +0000

    Deploying to gh-pages from @ KhelemeliaOleksii/SB-team-project@e2a817b80a7fa7e95578da4bdb6af0c2ace04075 🚀

--- src.d683b8d5.js
+++ src.d683b8d5.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/mobile-menu");
 },{"./sass/main.scss":"clu1","./js/mobile-menu":"sByI"}]},{},["Focm"], null)
-//# sourceMappingURL=/SB-team-project/src.997ef49d.js.map
+//# sourceMappingURL=/SB-team-project/src.d683b8d5.js.map
